9c2d73a234b751d52261ab482593f00ec190c6df
Neal Granger <neal@nealg.com>
2018-05-04 00:35:29 -0700

    Add es6-error dependency

--- src/SubmitValidationError.js
+++ src/SubmitValidationError.js
@@ -1,49 +1,16 @@
 // @flow strict
 
+import ExtendableError from 'es6-error';
 import type {State} from './types';
 
-export default class SubmitValidationError extends Error {
+export default class SubmitValidationError extends ExtendableError {
   errors: State;
   original: Error | void;
 
   constructor(errors: State, original?: Error) {
-    const message = 'Submit Validation Failed';
-    super(message);
+    super('Submit Validation Failed');
 
-    Object.defineProperties(this, {
-      message: {
-        enumerable: false,
-        value: message,
-      },
-      name: {
-        enumerable: false,
-        value: this.constructor.name,
-      },
-      errors: {
-        enumerable: false,
-        value: errors,
-      },
-      original: {
-        enumerable: false,
-        value: original,
-      },
-    });
-
-    if (Error.hasOwnProperty('captureStackTrace')) {
-      Error.captureStackTrace(this, this.constructor);
-      return;
-    }
-
-    Object.defineProperty(this, 'stack', {
-      configurable: true,
-      enumerable: false,
-      value: new Error(message).stack,
-      writable: true,
-    });
+    this.original = original;
+    this.errors = errors;
   }
 }
-
-export const unwrapError = (error: Error) =>
-  error instanceof SubmitValidationError
-    ? [error.original, error]
-    : [error, null];
