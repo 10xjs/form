629906499124be9f8182358d0a57903a45646039
Neal Granger <neal@nealg.com>
2018-05-03 19:47:11 -0700

    Enable flow strict

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-// @flow
+// @flow strict
 
 import * as React from 'react';
 import type {Context} from './types';
--- src/withField.js
+++ src/withField.js
@@ -1,4 +1,4 @@
-// @flow
+// @flow strict
 
 import * as React from 'react';
 // flowlint untyped-import:off
