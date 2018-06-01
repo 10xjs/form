bdf316e6fbdc10c58bd71a0c6fc7387df1a9495e
Neal Granger <neal@nealg.com>
2018-05-31 17:31:22 -0700

    Export path utils
    These functions are very useful to the consumer for working with form value and error state.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export {default as FieldArray} from './FieldArray';
 export {default as Form} from './Form';
 export {default as SubmitValidationError} from './SubmitValidationError';
 export {default as withField} from './withField';
+export {get, set, parsePath, formatPath} from './util';
 
 export type {
   State,
